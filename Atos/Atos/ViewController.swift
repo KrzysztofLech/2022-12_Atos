4f9341dd30e236d3f606bacfd665759fd7cb2063
Krzysztof Lech <krzysztof.lech@twoupdigital.com>
2022-12-28 15:17:43 +0100

    Xcode init files

---
+++ Atos/Atos/ViewController.swift
@@ -0,0 +1,15 @@
+//  ViewController.swift
+//  Created by Krzysztof Lech on 28/12/2022.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
